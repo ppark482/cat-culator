4269ec19c9e76c402ba5a8bb068ef9a1a63446d2
Paul Park <ppark482@gmail.com>
2014-09-30 14:56:18 -0400

    completed javascript

---
+++ main.js
@@ -0,0 +1,10 @@
+
+var output,
+first = document.getElementById('num_one'),
+second = document.getElementById('num_two'),
+submit = document.getElementById('calculateBtn'), solution = document.getElementById('answer');
+
+submit.onclick = function () {
+  output = Number(first.value) + Number(second.value);
+  solution.innerHTML = output;
+}
